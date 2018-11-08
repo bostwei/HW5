3c3772fd7a8e826cd51eaceac8b19e44d70ed8f4
bostwei <bostwei2011@gmail.com>
2018-11-08 10:18:47 -0600

    This version I treat the l as the function of a aa and z. so I flattening them to the same dimention of the (a,aa) space.

--- HW5.m
+++ HW5.m
@@ -7,6 +7,9 @@
 % ************************************************************************
 % This script is used to solving the homework 5 for Econ 899
 % ***********************************************************************
+clear
+clc
+close all
 
 %% Data Initiated 
 N = 66;
@@ -93,3 +96,55 @@ end
 plot(A,v1_r(:,4))
 
 %----------- The utility function for worked agent ----------------
+e = z * ef';
+
+v0_w_zh = zeros(Na,JR);
+v0_w_zl = zeros(Na,JR);
+v1_w_zh = zeros(Na,JR);
+v1_w_zl = zeros(Na,JR);
+% the last period of value function of working agent is the same as the
+% retire people
+
+v0_w_zh(:,JR) = v0_r(:,1);
+v0_w_zl(:,JR) = v0_r(:,1);
+v1_w_zh = v0_w_zh;
+v1_w_zl = v0_w_zl;
+
+
+dec_w_zh = zeros(Na,JR-1);
+dec_w_zl = zeros(Na,JR-1);
+
+for j = JR-1:-1:1
+    
+% ------------ the labor choice of worker --------------------
+% labor efficiency 
+% - first row is ef if it get z = 3
+% - second row is the ef if it get z = 0.5
+
+
+l_zh = (ggama*(1-ttheta) * e(1,j) * w - (1-ggama)*((1+r)*a - aa'))/((1-ttheta)* w *e(1,j)); 
+l_zl = (ggama*(1-ttheta) * e(2,j) * w - (1-ggama)*((1+r)*a - aa'))/((1-ttheta)* w *e(2,j));
+l_zh(find(l_zh <0)) = NaN;
+l_zl(find(l_zl <0)) = NaN;
+
+% consumption of worker
+    
+  c_w_zh = w * (1-ttheta)* e(1,j)* l_zh + (1+r) * a - aa';
+  c_w_zl = w * (1-ttheta)* e(2,j)* l_zl + (1+r) * a - aa';
+  
+% utility of the worker is 
+ u_w_zh = (c_w_zh.^ggama.*(1-l_zh).^(1-ggama)).^(1-ssigma)/(1-ssigma);
+ u_w_zl = (c_w_zl.^ggama.*(1-l_zl).^(1-ggama)).^(1-ssigma)/(1-ssigma);
+ u_w_zh(find(isnan(u_w_zh))) = -inf;
+ u_w_zl(find(isnan(u_w_zl))) = -inf;
+ 
+ % optimization choose of aa'
+ u_w_zh + bbeta * (
+ 
+% c_r(find(c_r <=0)) = NaN;
+% 
+% % utility of the retirement
+% u_r = (c_r .^ ((1-ssigma) .* ggama))./(1-ssigma);
+% u_r(find(isnan(u_r))) = -inf;
+end
+
