d1105ca5b0165c23b111cb26645c5ba7c3fc53af
swei33 <swei33@RDSTS-107.ads.ssc.wisc.edu>
2018-11-14 14:47:16 -0600

    In this version, I fixed a and choose a' and l.

--- HW5.m
+++ HW5.m
@@ -95,67 +95,38 @@ end
 
 plot(A,v1_r(:,4))
 
-%----------- For working agent ----------------
-% Generate the working efficiency
+%----------- The utility function for worked agent ----------------
 % labor efficiency 
 % - first row is ef if it get z = 3
 % - second row is the ef if it get z = 0.5
 e = z * ef';
 
-% initiate the value function
 v0_w_zh = zeros(Na,JR);
 v0_w_zl = zeros(Na,JR);
 v1_w_zh = zeros(Na,JR);
 v1_w_zl = zeros(Na,JR);
-
-% initiate the decesion rule
-dec_w_zh = zeros(Na,JR-1);
-dec_w_zl = zeros(Na,JR-1);
-
-% Generate the value function for the last period working agent
-% - the last period of value function of working agent is the same as the
-% - retire people
+% the last period of value function of working agent is the same as the
+% retire people
 
 v0_w_zh(:,JR) = v0_r(:,1);
 v0_w_zl(:,JR) = v0_r(:,1);
 v1_w_zh = v0_w_zh;
 v1_w_zl = v0_w_zl;
 
-% the labor choice of worker
-
-
-
-l_zh = (ggama*(1-ttheta) * e(1,JR-1) * w - (1-ggama)*((1+r)*a - aa'))/((1-ttheta)* w *e(1,JR-1)); 
-l_zl = (ggama*(1-ttheta) * e(2,JR-1) * w - (1-ggama)*((1+r)*a - aa'))/((1-ttheta)* w *e(2,JR-1));
-l_zh(find(l_zh <0)) = NaN;
-l_zl(find(l_zl <0)) = NaN;
-
-% consumption of worker
+% searh for the best l an aa' combination for each a
+for ai = 1:Na
     
-  c_w_zh = w * (1-ttheta)* e(1,JR-1)* l_zh + (1+r) * a - aa';
-  c_w_zl = w * (1-ttheta)* e(2,JR-1)* l_zl + (1+r) * a - aa';
-  
-% utility of the worker is 
- u_w_zh = (c_w_zh.^ggama.*(1-l_zh).^(1-ggama)).^(1-ssigma)/(1-ssigma);
- u_w_zl = (c_w_zl.^ggama.*(1-l_zl).^(1-ggama)).^(1-ssigma)/(1-ssigma);
- u_w_zh(find(isnan(u_w_zh))) = -inf;
- u_w_zl(find(isnan(u_w_zl))) = -inf;
- 
-% generate the wealth function include the future retirement value 
-w_w_zh = u_w_zh + bbeta * (v0_w_zh(:,JR));
-w_w_zl = u_w_zl + bbeta * (v0_w_zl(:,JR));
+end
 
-% Generate th choice of the last period woring agent
-[v1_w_zh(:,JR-1), dec_w_zh(:,JR-1)] = max(w_w_zh,[],2);
-[v1_w_zl(:,JR-1), dec_w_zl(:,JR-1)] = max(w_w_zl,[],2);
 
-% For the JR - 2 worker
+dec_w_zh = zeros(Na,JR-1);
+dec_w_zl = zeros(Na,JR-1);
+
 for j = JR-1:-1:1
     
 % ------------ the labor choice of worker --------------------
-% labor efficiency 
-% - first row is ef if it get z = 3
-% - second row is the ef if it get z = 0.5
+
+
 
 
 l_zh = (ggama*(1-ttheta) * e(1,j) * w - (1-ggama)*((1+r)*a - aa'))/((1-ttheta)* w *e(1,j)); 
@@ -174,16 +145,8 @@ l_zl(find(l_zl <0)) = NaN;
  u_w_zh(find(isnan(u_w_zh))) = -inf;
  u_w_zl(find(isnan(u_w_zl))) = -inf;
  
-% generate the wealth function include the future retirement value 
-w_w_zh = u_w_zh + bbeta * (v0_w_zh(:,JR))';
-w_w_zl = u_w_zl + bbeta * (v0_w_zh(:,JR))';
-
-% Generate th choice of the last period woring agent
-[v1_w_zh(:,JR-1), dec_w_zh(:,JR-1)] = max(w_w_zh,[],2);
-
- 
  % optimization choose of aa'
-%  u_w_zh + bbeta * (
+ u_w_zh + bbeta * (
  
 % c_r(find(c_r <=0)) = NaN;
 % 
